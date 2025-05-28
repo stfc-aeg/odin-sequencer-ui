2c52c6565513bad2c9f4b7201c83a4500fd87f3c
william cross <william.cross@stfc.ac.uk>
2025-05-28 13:59:23 +0100

    removed URL from the library
    The URL is to be set when starting the app that uses this using the terminal.
    
    The URL does not need to be set in production as leaving it empty should fill it in for the API

--- lib/components/sequencerEndpoint.jsx
+++ lib/components/sequencerEndpoint.jsx
@@ -1,6 +1,6 @@
 import { AdapterEndpoint } from './AdapterEndpointWrapper';
 
-const ENDPOINT_URL = import.meta.env.VITE_SEQUENCER_ENDPOINT_URL || 'http://127.0.0.1:8888';
+const ENDPOINT_URL = import.meta.env.VITE_SEQUENCER_ENDPOINT_URL;
 console.log(ENDPOINT_URL)
 const sequencer_endpoint = new AdapterEndpoint("odin_sequencer", ENDPOINT_URL);
 
