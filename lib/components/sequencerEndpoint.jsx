f4e4948dd8fdde295a38d56d20c7b2e83d96f5ae
william cross <william.cross@stfc.ac.uk>
2025-05-28 11:10:10 +0100

    Changed the name of the .env endpoint url name. Changed .env to .env.production

--- lib/components/sequencerEndpoint.jsx
+++ lib/components/sequencerEndpoint.jsx
@@ -1,6 +1,7 @@
 import { AdapterEndpoint } from './AdapterEndpointWrapper';
 
-const ENDPOINT_URL = import.meta.env.VITE_ENDPOINT_URL;
+const ENDPOINT_URL = import.meta.env.VITE_SEQUENCER_ENDPOINT_URL || 'http://127.0.0.1:8888';
+console.log(ENDPOINT_URL)
 const sequencer_endpoint = new AdapterEndpoint("odin_sequencer", ENDPOINT_URL);
 
 export default sequencer_endpoint;
